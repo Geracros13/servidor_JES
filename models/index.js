1ac60e5dc93432cb9323e6bc024cdd2419056353
Gerardo Quiñonez <gerardo01302011@gmail.com>
2020-08-30 13:11:54 -0600

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema; //Descripcion de que es lo que queremos guardar en la BD
 
 newSchema = new Schema({
   name: String,
