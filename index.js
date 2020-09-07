2e3f9967fe49250a55fc830ff5348fe4c3d4d407
Flores <gerardo01302011@gmail.com>
2020-09-07 18:06:50 -0500

    Agregando Documentacion a la api con Swagger

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require("express");//Framework de servidor 
-const mongoose = require("mongoose");//Modulo de conexion a la BD de mongo
+const express = require("express"); //Framework de servidor
+const mongoose = require("mongoose"); //Modulo de conexion a la BD de mongo
 const cors = require("cors");
 
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 //Importar variables de entorno
 require("dotenv").config({ path: "variables.env" });
 
 //Sirve para conectarse a la BD
-mongoose.connect(process.env.DB_URL, {//Asignamos la variable de entorno de la DB
+mongoose.connect(process.env.DB_URL, {
+  //Asignamos la variable de entorno de la DB
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -18,6 +21,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api", require("./routes/index"));
+
+//Swagger
+//Inicio de la documentacion, puedo consultar la sintaxis aqui [https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#serverObject]
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Backend para el Proyecto de Fiscalias",
+      description: "CRUD de proyecto de JES",
+      servers: ["https://polar-mountain-90943.herokuapp.com/api"],
+    },
+  },
+  apis: ["./routes/index.js"], //Aqui defino en donde estan la api, por ejemplo las rutas
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/documentacion", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+//Swagger
 
 // Leer localhost de variables y puerto
 const host = process.env.HOST || "0.0.0.0";
--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,139 @@ const express = require("express");
 const User = require("../models/index"); //Importamos la BD
 const router = express.Router(); //Direccionamiento hace referencia a la definición de puntos finales de aplicación (URI
 
+/**
+ * @swagger
+ *
+ * /api:
+ *  get:
+ *      description: Creacion de Fiscalias
+ *      responses:
+ *        200:
+ *           description: Se agrego con exito la fiscalia
+ */
+
 router.get("/", (req, res) => {
   User.find({}, (err, data) => {
     res.json(data);
   });
 });
 
-router.get("/:id", (req, res) => { // rutas dinámicas : y lo que queremos recibir :id
-  User.findById(req.params.id, (err, data) => { // params, parámetro de la petición 
+/**
+ * @swagger
+ *
+ * /api/{id}:
+ *  get:
+ *      description: Consultar por ID
+ *      parameters:
+ *          - name: id
+ *            description: ID de la fiscalia
+ *            in: path
+ *            type: string
+ *            required: true
+ *      responses:
+ *        200:
+ *           description: Se agrego con exito la fiscalia
+ */
+router.get("/:id", (req, res) => {
+  // rutas dinámicas : y lo que queremos recibir :id
+  User.findById(req.params.id, (err, data) => {
+    // params, parámetro de la petición
     res.json(data); // Especificar que vamos a devolver un objeto json
   });
 });
 
+/**
+ * @swagger
+ *
+ * /api/{id}:
+ *  delete:
+ *      description: Borrar Fiscalia por ID
+ *      parameters:
+ *          - name: id
+ *            description: ID de la fiscalia a borrar
+ *            in: path
+ *            type: string
+ *            required: true
+ *      responses:
+ *        200:
+ *           description: Se borro con exito la fiscalia
+ */
 router.delete("/:id", async (req, res) => {
   await User.findByIdAndDelete(req.params.id);
   res.json({ message: "Registro borrado" });
 });
 
+/**
+ * @swagger
+ *
+ * /api:
+ *  post:
+ *      description: Crear Fiscalia
+ *      parameters:
+ *          - name: Nombre
+ *            description: Nombre de la fiscalia
+ *            in: body
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  name:
+ *                    type: string
+ *                  ubicacion:
+ *                    type: string
+ *                  telefono:
+ *                    type: number
+ *              required:
+ *                  - name
+ *                  - ubicacion
+ *                  - telefono
+ *      responses:
+ *        200:
+ *           description: Se creo con exito la fiscalia
+ */
 router.post("/", (req, res) => {
   user = new User({
-    name: req.body.name, // body recibe los datos que me esta enviando el frontend cuerpo de la petición 
+    name: req.body.name, // body recibe los datos que me esta enviando el frontend cuerpo de la petición
     ubicacion: req.body.ubicacion,
     telefono: req.body.telefono,
   });
-  user.save(() => {//Para guardar en la BD 
+  user.save(() => {
+    //Para guardar en la BD
     res.json(user);
   });
 });
 
+/**
+ * @swagger
+ *
+ * /api/{id}:
+ *  put:
+ *      description: Actualizar Fiscalia
+ *      parameters:
+ *          - name: id
+ *            description: ID de la fiscalia a actualizar
+ *            in: path
+ *            type: string
+ *            required: true
+ *          - name: Nombre
+ *            description: Nombre de la fiscalia
+ *            in: body
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  name:
+ *                    type: string
+ *                  ubicacion:
+ *                    type: string
+ *                  telefono:
+ *                    type: number
+ *              required:
+ *                  - name
+ *                  - ubicacion
+ *                  - telefono
+ *      responses:
+ *        200:
+ *           description: Se actualizo con exito la fiscalia
+ */
 router.put("/:id", async (req, res) => {
   await User.findByIdAndUpdate(req.params.id, req.body);
   res.json({ message: "Registro Actualizado" });
